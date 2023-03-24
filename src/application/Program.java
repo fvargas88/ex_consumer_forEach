c2760c4fa68453adc7ae0e09bf3937fbdb5a51ef
fvargas88 <fvargas_88@hotmail.com>
2023-03-24 12:21:34 -0300

    Versao com expressao lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
+import java.util.function.Consumer;
 
 import entites.Product;
-import util.PriceUpdate;
 
 public class Program {
 
@@ -20,7 +19,13 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product :: nonStaticPriceUpdate);
+		double factor = 1.1;
+		Consumer<Product> cons = p -> {
+			// {} opcionais pois é somente uma linha
+			p.setPrice(p.getPrice() * factor);
+		};
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 
